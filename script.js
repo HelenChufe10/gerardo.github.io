3af7907f343f96d0a3ca87d86de3993ca668afba
HelenChufe10 <6813417@alu.murciaeduca.es>
2025-03-17 14:49:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var edad=prompt("¿Cual es tu edad?")
+if (edad > 20){
+  alert("adulto")
+}else{
+  if (edad>=16){
+  alert("Eres adolescente")
+}else{
+alert("Eres un niño")
+ }
+}
+alert( "he terminado")
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
